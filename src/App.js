022d4057a791484064e2c85b0e1cfc012b2cbaf5
madinajabrayil <madi.jabra13@gmail.com>
2021-04-12 04:59:34 +0400

    First test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import Header from './commons/Header/Header';
+import { Footer } from './commons/Footer/Footer';
+import Router from './routes/route';
+import store from './store/';
+import { BrowserRouter} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+        <Header />
+        < Router />
+        </BrowserRouter>
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
